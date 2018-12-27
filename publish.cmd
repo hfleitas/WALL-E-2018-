9e91d6677352a3c395ea78267c6f6593fe4d07da
hiramfleitas <hiramfleitas@hotmail.com>
2018-12-26 19:06:47 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=wall-e2018-Web-Deploy.pubxml -p:Password=flcY4ocs4oguNraNFT1TrHs0gQbgRHY0zZJxWbyk2GhgZhgutLKA2SxblDJH
+
